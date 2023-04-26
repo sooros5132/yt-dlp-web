5beeda7c6688929ab20ec056cecbd9252feefb84
SOOROS <sooros5132@gmail.com>
2023-04-26 14:09:01 +0900

    metadata, og image 추가

---
+++ src/app/api/og/route.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { ImageResponse } from '@vercel/og';
+
+export const config = {
+  runtime: 'edge'
+};
+
+export function GET() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          textAlign: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100%',
+          height: '100%',
+          background:
+            'linear-gradient(to right top, #973c70, #933e80, #8a4390, #7b49a0, #6451b0, #5261bf, #3970cc, #007ed6, #0095dd, #00a9da, #00bbce, #1fcbc0)',
+          color: 'white',
+          fontSize: 128,
+          fontWeight: 'bold',
+          textShadow: '3px 4px 5px #202034'
+        }}
+      >
+        📹 yt-dlp-web
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600
+    }
+  );
+}
